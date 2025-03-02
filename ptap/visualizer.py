87816b327dc9b9b4f749e9ef2e59fddf6ff96e78
Far3000-YT <far3000yt@gmail.com>
2025-03-02 23:42:36 +0100

    working on visualizer

--- ptap/visualizer.py
+++ ptap/visualizer.py
@@ -3,18 +3,60 @@ from typing import List
 
 
 def get_project_structure(root_path: str, skipped_folders: List):
-    structure = []
+    root_path_name = "".join(root_path.split("/")[-1] + "/")
+    print(root_path_name)
+    structure = {root_path_name: {}}
 
     for root, directories, files in os.walk(root_path, topdown = True):
         if any(root.endswith(folder) for folder in skipped_folders):
             directories[:] = []
+            structure[root_path_name][root.split("/")[-1]] = {}
             continue
 
-        print(root, directories, files)
+        base = structure[root_path_name]
+        level = len(root.split("\\")) - len(root_path.split("\\"))
+        print(level, "level")
+        for x in range(level, 0, -1):
+            folder_subname = root.split("\\")[-x]
+            print(root.split("\\"))
+            print(folder_subname)
+            print(base)
+            print(base[folder_subname])
+            base = base[folder_subname]
 
-    return structure
+        print(f"BASE IS : {base}")
+        print(root_path.split("\\"), 1)
+        print(root, "\n", root.split("\\"), 2,  "\n\n")
 
-get_project_structure("C:/Users/far30/Downloads/GENERATION MEMOIRE TECHNIQUE API", [".git", "__pycache__"])
+        #folder_level = len(structure[root_path_name][root.split("/")])
+
+        '''if index == folders_num:
+            index = 0
+            folder_level += 1
+            folders_num = 0'''
+
+        #print(root, directories, files)
+        
+        #add folders
+        #add files
+        #iterate through folders
+        #add folders and files again until we can't :)
+        
+        actual_directory = root.split()
+
+        '''if directories:
+            files_index += 1
+            for directory in directories:
+                structure[root_path_name][str({directory}) + "/"] = {}
+        
+        if files:
+            for file in files:
+                pass'''
+        
+
+    print(structure)
+
+get_project_structure(root_path = "C:/Users/far30/Downloads/GENERATION MEMOIRE TECHNIQUE API", skipped_folders = [".git", "__pycache__"])
 
 
 """
