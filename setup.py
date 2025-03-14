3a49ead59b078d68a3fad2cb2fc1f6758fd86e42
Far3000-YT <far3000yt@gmail.com>
2025-03-14 23:59:22 +0100

    release pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name = "ptap",
-    version = "v1.0",
+    version = "v1.0.1",
     packages = find_packages(include = ["ptap", "ptap.*"]),
     entry_points = {
         "console_scripts": [
